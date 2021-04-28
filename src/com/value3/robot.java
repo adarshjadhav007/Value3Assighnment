9e60fe551d8eaa45ebf9cf6d7ca0417ce4b7e201
Adarsh <adarshjadhav007@gmail.com>
2021-04-28 20:45:53 +0530

    Hello

--- src/com/value3/robot.java
+++ src/com/value3/robot.java
@@ -9,48 +9,53 @@ import java.util.Map;
 
 public class robot {
 	public static void main(String[] args) {
-
-		String[] input = args[0].split(","); 
-		int x= Integer.parseInt(input[0].trim());
-		int y= Integer.parseInt(input[1].trim());
-		String dir = input[2];
-		
-		Map<String, List<String>> diractionMap = new HashMap<>();
-		diractionMap.put("NORTH",Arrays.asList("WEST", "EAST"));
-		diractionMap.put("EAST",Arrays.asList("NORTH", "SOUTH"));
-		diractionMap.put("WEST",Arrays.asList("SOUTH", "NORTH"));
-		diractionMap.put("SOUTH",Arrays.asList("EAST", "WEST"));
-		
-		List<String> sides = Arrays.asList("LEFT", "RIGHT");
-		
-		
-		
-		for(int i=1; i<args.length ; i++) {
-			
-			if(args[i].toUpperCase().equals("REPORT")) {
-				System.out.println(x + "," + y + "," +dir );
-				break;
-			}
-			
-			if(args[i].toUpperCase().equals("MOVE")) {
-				if(x<5 && y<5) {
-					if(dir.toUpperCase().equals("NORTH"))
-							y = y + 1;
-						else if (dir.toUpperCase().equals("EAST")) 
-							x = x + 1;
-						else if (dir.toUpperCase().equals("SOUTH")) 
-							y = y - 1;
-						else if (dir.toUpperCase().equals("WEST")) 
-							x = x - 1;	
-				}
-						
-			} else {
-				try {
-					dir = diractionMap.get(dir).get(sides.indexOf(args[i]));
-				} catch (Exception e) {
-					System.out.println("Please Enter Valid input");
-				}
-			}
+		if(args.length>0)
+		{
+			try {
+				if(args[0].contains("PLACE")) {
+				String[] input = args[0].replace("PLACE ","").split(","); 
+				int x= Integer.parseInt(input[0].trim());
+				int y= Integer.parseInt(input[1].trim());
+				String dir = input[2];
+				
+				Map<String, List<String>> diractionMap = new HashMap<>();
+				diractionMap.put("NORTH",Arrays.asList("WEST", "EAST"));
+				diractionMap.put("EAST",Arrays.asList("NORTH", "SOUTH"));
+				diractionMap.put("WEST",Arrays.asList("SOUTH", "NORTH"));
+				diractionMap.put("SOUTH",Arrays.asList("EAST", "WEST"));
+				
+				List<String> sides = Arrays.asList("LEFT", "RIGHT");
+				
+				
+				
+				for(int i=1; i<args.length ; i++) {
+					
+					if(args[i].toUpperCase().equals("REPORT")) {
+						System.out.println(x + "," + y + "," +dir );
+						break;
+					}
+					
+					if(args[i].toUpperCase().equals("MOVE")) {
+						if(x<4 && y<4 && x>0 && y>0) {
+							if(dir.toUpperCase().equals("NORTH"))
+									y = y + 1;
+								else if (dir.toUpperCase().equals("EAST")) 
+									x = x + 1;
+								else if (dir.toUpperCase().equals("SOUTH")) 
+									y = y - 1;
+								else if (dir.toUpperCase().equals("WEST")) 
+									x = x - 1;	
+						}
+								
+					} else {
+					
+							dir = diractionMap.get(dir).get(sides.indexOf(args[i]));
+						} }}}
+					catch (Exception e) {
+							System.out.println("Please Enter Valid input");
+						}
+					}
 		}	
+	
 	}
-}
+
--- src/module-info.java
+++
@@ -1,2 +0,0 @@
-module Value3_Robot_test {
-}
