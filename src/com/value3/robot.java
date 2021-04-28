3bc903b0b80e9493e7821f3c010e2e0f134b5958
Adarsh <adarshjadhav007@gmail.com>
2021-04-29 01:46:34 +0530

    updated code

--- src/com/value3/robot.java
+++ src/com/value3/robot.java
@@ -8,54 +8,65 @@ import java.util.Map;
 
 
 public class robot {
+	
 	public static void main(String[] args) {
-		if(args.length>0)
-		{
+		if(args.length>0) {
 			try {
 				if(args[0].contains("PLACE")) {
-				String[] input = args[0].replace("PLACE ","").split(","); 
-				int x= Integer.parseInt(input[0].trim());
-				int y= Integer.parseInt(input[1].trim());
-				String dir = input[2];
-				
-				Map<String, List<String>> diractionMap = new HashMap<>();
-				diractionMap.put("NORTH",Arrays.asList("WEST", "EAST"));
-				diractionMap.put("EAST",Arrays.asList("NORTH", "SOUTH"));
-				diractionMap.put("WEST",Arrays.asList("SOUTH", "NORTH"));
-				diractionMap.put("SOUTH",Arrays.asList("EAST", "WEST"));
-				
-				List<String> sides = Arrays.asList("LEFT", "RIGHT");
-				
-				
-				
-				for(int i=1; i<args.length ; i++) {
-					
-					if(args[i].toUpperCase().equals("REPORT")) {
-						System.out.println(x + "," + y + "," +dir );
-						break;
-					}
-					
-					if(args[i].toUpperCase().equals("MOVE")) {
-						if(x<4 && y<4 && x>0 && y>0) {
-							if(dir.toUpperCase().equals("NORTH"))
-									y = y + 1;
-								else if (dir.toUpperCase().equals("EAST")) 
-									x = x + 1;
-								else if (dir.toUpperCase().equals("SOUTH")) 
-									y = y - 1;
-								else if (dir.toUpperCase().equals("WEST")) 
-									x = x - 1;	
+					String[] input = args[1].split(","); 
+					int x= Integer.parseInt(input[0].trim());
+					int y= Integer.parseInt(input[1].trim());
+					String dir = input[2];
+					if(x<4 && y<4 && x>=0 && y>=0) {
+						Map<String, List<String>> diractionMap = new HashMap<>();
+						diractionMap.put("NORTH",Arrays.asList("WEST", "EAST"));
+						diractionMap.put("EAST",Arrays.asList("NORTH", "SOUTH"));
+						diractionMap.put("WEST",Arrays.asList("SOUTH", "NORTH"));
+						diractionMap.put("SOUTH",Arrays.asList("EAST", "WEST"));
+						
+						List<String> sides = Arrays.asList("LEFT", "RIGHT");
+						
+						for(int i=2; i<args.length ; i++) {
+							
+							if(args[i].toUpperCase().equals("REPORT")) {
+								System.out.println(x + "," + y + "," +dir );
+								break;
+							}
+							
+							if(args[i].toUpperCase().equals("MOVE")) {
+								if(x<4 && y<4 && x>=0 && y>=0) {
+									if(dir.toUpperCase().equals("NORTH"))
+											y = y + 1;
+										else if (dir.toUpperCase().equals("EAST")) 
+											x = x + 1;
+										else if (dir.toUpperCase().equals("SOUTH")) 
+											y = y - 1;
+										else if (dir.toUpperCase().equals("WEST")) 
+											x = x - 1;	
+								}
+										
+							} else {
+								if(sides.contains(args[i].toUpperCase())) {
+									dir = diractionMap.get(dir).get(sides.indexOf(args[i].toUpperCase()));
+								}else {
+									System.out.println("PLeas valid Direction");
+								}
+							} 
 						}
-								
-					} else {
-					
-							dir = diractionMap.get(dir).get(sides.indexOf(args[i]));
-						} }}}
-					catch (Exception e) {
-							System.out.println("Please Enter Valid input");
-						}
-					}
-		}	
+					}else {
+						System.out.println("initial position is misplaced");
+					}	
+				} else {
+					System.out.println("'PLACE' Command is missing!");
+				}
+			}
+			catch (Exception e) {
+				System.out.println("Please Enter Valid input");
+			}
+		} else {
+			System.out.println("Please Enter Input");
+		}
+	}	
 	
-	}
+}
 
