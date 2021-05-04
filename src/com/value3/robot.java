79db1c4649af83bdb99f242396164dae49977a95
Adarsh <adarshjadhav007@gmail.com>
2021-05-04 20:15:20 +0530

    bug fix

--- src/com/value3/robot.java
+++ src/com/value3/robot.java
@@ -5,76 +5,56 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.value3.service.RobotService;
+
 
 
 public class robot {
 	
 	public static void main(String[] args) {
-		if(args.length>0) {
-			try {
-				if(args[0].contains("PLACE")) {
-					String[] input = args[1].split(","); 
-					int x= Integer.parseInt(input[0].trim());
-					int y= Integer.parseInt(input[1].trim());
-					
-						if(x<4 && y<4 && x>=0 && y>=0) {
-						Map<String, List<String>> diractionMap = new HashMap<>();
-						diractionMap.put("NORTH",Arrays.asList("WEST", "EAST"));
-						diractionMap.put("EAST",Arrays.asList("NORTH", "SOUTH"));
-						diractionMap.put("WEST",Arrays.asList("SOUTH", "NORTH"));
-						diractionMap.put("SOUTH",Arrays.asList("EAST", "WEST"));
-						
-						String dir = "";
-						if(diractionMap.get(input[2].trim())!=null) {
-							dir = input[2];
-						} else {
-							System.out.println("Initial Direction is invalid");
-							System.exit(0);
+		
+		int MaxBoxSizeX = 5;
+		int MaxBoxSizeY = 5;
+		int MinBoxSize = 0;
+		int x = 0;
+		int y = 0;
+		String side = "";
+		Boolean start = false;
+		List<String> directions = Arrays.asList("LEFT", "RIGHT");
+		
+		RobotService service = new RobotService();
+		
+		for(int i=0; i<args.length ; i++) {
+			if(args[i].toUpperCase().equals("PLACE")) {
+				
+				List<Object> initialPosition = service.InitialPlace(args[i+1], x, y, side, MaxBoxSizeY, MaxBoxSizeY, MinBoxSize);
+				x = (int) initialPosition.get(0);
+				y = (int) initialPosition.get(1);
+				side = initialPosition.get(2).toString().toUpperCase();
+				start = (Boolean) initialPosition.get(3);
+				i=i+1;
+				
+			} else {
+				if(start) {
+					if(args[i].toUpperCase().equals("MOVE")) {
+						List<Integer> move = service.Move(x, y, side);
+						if(move.get(0)<MaxBoxSizeX && move.get(1)<MaxBoxSizeY && move.get(0)>=MinBoxSize && move.get(1)>=MinBoxSize) {
+							x = move.get(0);
+							y = move.get(1);
 						}
-						
-						List<String> sides = Arrays.asList("LEFT", "RIGHT");
-						
-						for(int i=2; i<args.length ; i++) {
-							
-							if(args[i].toUpperCase().equals("REPORT")) {
-								System.out.println(x + "," + y + "," +dir );
-								break;
-							}
-							
-							if(args[i].toUpperCase().equals("MOVE")) {
-								if(x<4 && y<4 && x>=0 && y>=0) {
-									if(dir.toUpperCase().equals("NORTH"))
-											y = y + 1;
-										else if (dir.toUpperCase().equals("EAST")) 
-											x = x + 1;
-										else if (dir.toUpperCase().equals("SOUTH")) 
-											y = y - 1;
-										else if (dir.toUpperCase().equals("WEST")) 
-											x = x - 1;	
-								}
-										
-							} else {
-								if(sides.contains(args[i].toUpperCase())) {
-									dir = diractionMap.get(dir).get(sides.indexOf(args[i].toUpperCase()));
-								}else {
-									System.out.println("Pleas enter valid Input");
-									System.exit(0);
-								}
-							} 
-						}
+					}else if ( directions.contains(args[i].toUpperCase())) {
+						side = service.changeDirection(directions, side, args[i].toUpperCase());
+					} else if (args[i].toUpperCase().equals("REPORT")) {	
+						service.report(x, y, side);	
 					}else {
-						System.out.println("initial position is misplaced");
-					}	
-				} else {
-					System.out.println("'PLACE' Command is missing!");
+						System.out.println(args[i] + " is not valid command");
+					}
+						
 				}
 			}
-			catch (Exception e) {
-				System.out.println("Please Enter Valid input");
-			}
-		} else {
-			System.out.println("Please Enter Input");
 		}
+		
+		
 	}	
 	
 }
